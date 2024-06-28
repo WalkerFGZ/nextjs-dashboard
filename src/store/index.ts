10c5055482800e77952869d1e0af1e0b49bfed81
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2024-06-27 19:43:42 -0500

    feat: Add counterSlice to store and configure counterReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter/counterSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counterReducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
