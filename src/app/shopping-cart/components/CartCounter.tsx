ec46e98fe07bfd7cb8b9de38a5d6605b94771393
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2024-06-29 21:51:03 -0500

    chore: Update CartCounter component to initialize count with a value

--- src/app/shopping-cart/components/CartCounter.tsx
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { useState } from "react";
+import {
+  addOne,
+  decreaseOne,
+  initCounterState,
+} from "@/store/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "@/store";
 
-export const CartCounter = () => {
-  const [count, setCount] = useState(5);
+import { useEffect } from "react";
+
+export const CartCounter = ({ value = 0 }) => {
+  const count = useAppSelector((state) => state.counter.count);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(initCounterState(value));
+  }, [dispatch, value]);
   return (
     <>
       <span className="text-9xl">{count}</span>
@@ -11,14 +23,14 @@ export const CartCounter = () => {
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600
         transition-all w-[100px] mr-2"
-          onClick={() => setCount(count + 1)}
+          onClick={() => dispatch(addOne())}
         >
           +1
         </button>
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600
         transition-all w-[100px] mr-2"
-          onClick={() => setCount(count - 1)}
+          onClick={() => dispatch(decreaseOne())}
         >
           -1
         </button>
